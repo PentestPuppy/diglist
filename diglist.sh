d5f4625f17d99c838c3cc28a7c376d132533afd0
PentestPuppy <170636746+PentestPuppy@users.noreply.github.com>
2025-01-23 15:51:56 -0800

    Ask user for root domain, add check for results

--- diglist.sh
+++ diglist.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
-# Made by TRASH PUPPY 
+# Made by TRASH PUPPY
 #	2024
-#
 
 echo ""
 echo -e "                    \033[32m :-- DNS RECORDS --:\033[0m"
@@ -14,27 +12,38 @@ if [[ -z "$1" ]]; then
 	read -a ipfile
 else
 	ipfile=$1
-fi	
+fi
+
+if [[ -z "$2" ]]; then
+	echo ":-- Enter the root domain:"
+	read -a root
+else
+	root=$2
+fi
 
 # Check for what type of record they want:
-if [[ -z "$2" ]]; then
+if [[ -z "$3" ]]; then
 	echo ":-- What record type are you looking for? (ex: 'txt'): "
 	read -a record
 else
-	record=$2
+	record=$3
 fi
 
 echo -e ":-- Digging \033[32m$ipfile\033[0m for \033[32m$record\033[0m record types..."
 echo ""
 
 for line in $(cat $ipfile); do
-        echo -e "_________________________ \033[32m$line\033[0m _________________________"
+        echo -e "_________________________ \033[32m$line.$root\033[0m _________________________"
 
-	result=$(dig $line $record & sleep 0.5)
-	echo -e ":--\033[32mRESULT:\033[0m $result"	
-
+	# Check for results first
+	if [[ $(dig +short $line\.$root $record | wc -c) -eq 0 ]]; then
+		echo -e ":--\033[31m NO RESULTS for $line\033[0m"
+	else
+		result=$(dig $line\.$root $record)
+		echo -e ":--\033[32m RESULT:\033[0m $result" \
+		#&& sleep 0.1
+	fi
 done
 
 echo ""
 echo -e "                    \033[32m :-- DONE --:\033[0m"
-
